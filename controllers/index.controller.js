536cb0ee018420c9da4f697d39f3588de23632a1
Piotr Wolanin <wolanin.piotr1500@gmail.com>
2020-03-19 00:47:39 +0100

    first deploy

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const redirectLogin = require("../middlewares/authorisation").redirectLogin;
 
-router.get("/", redirectLogin, (req, res) => {
-  res.redirect("/books");
+router.get("/", (req, res) => {
+  res.end("Index page!");
+  // res.redirect("/books");
 });
 router.use("/books", redirectLogin, require("./books.controller"));
 router.use("/", require("./users.controller"));
